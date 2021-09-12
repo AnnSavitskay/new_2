5feeb2a6d0b96507ca936ee8eb743747c49ab914
Ann_Savitskay <ansavitskayloki@gmail.com>
2021-09-12 18:00:14 +0300

    new project

---
+++ src/new_22/New_22.java
@@ -0,0 +1,14 @@
+package new_22;
+
+import java.util.Scanner;
+
+class New_22
+{
+    public static void main(String[] args) {
+        String answer;
+        Scanner scan_ = new Scanner(System.in);
+        System.out.println("Enter the line");
+        answer = scan_.nextLine();
+        System.out.println(answer);
+    }
+}
